f905c19441560f9b4c5f51dca45aefbe71f2a733
Zeeshan <sk.zeeshan1992@gmail.com>
2019-10-27 14:51:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.get("/api", (_, res) => {
+  res.json({
+    message: "Welcome to the API"
+  });
+});
+
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
+
+// Verify Token
+const verifyToken = (req, res, next) => {
+  // Get auth header value
+  const bearerHeader = req.headers["authorization"];
+  // Check if bearer is undefined
+  if (typeof bearerHeader !== "undefined") {
+    // Split at the space
+    const bearer = bearerHeader.split(" ");
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+};
+
+app.post("/api/posts", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, authData) => {
+    return err
+      ? res.sendStatus(403)
+      : res.json({
+          message: "Post created...",
+          authData
+        });
+  });
+});
+
+app.post("/api/login", (_, res) => {
+  // Mock user
+  const user = {
+    id: 2,
+    username: "iZsk",
+    email: "izsk@outlook.com"
+  };
+
+  jwt.sign({ user }, "secretkey", { expiresIn: "30s" }, (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+
+app.listen(8000, () => console.log("Server started on port 8000"));
